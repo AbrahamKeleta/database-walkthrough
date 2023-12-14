e48576f0730d0631b062126180eee3157db7fc03
Abraham Keleta <keletaabraham@gmail.com>
2023-12-13 17:59:40 -0800

    sending code

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ class User(db.Model):
     name = db.Column(db.String(30))
     email = db.Column(db.String)
 
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
 
 if (__name__ == "__main__"): 
    app.run(debug=True)
